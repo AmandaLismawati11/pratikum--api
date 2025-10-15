03af4cad3527315d4a52f6d676f600e94f6b7432
amandalismawati11 <lismawatiamanda@gmail.com>
2025-10-15 10:17:09 +0700

    update

---
+++ README.md
@@ -0,0 +1,10 @@
+buat ccontroller
+php artisan make:controller
+
+migrate
+php artisan make:migration
+
+php artisan serve
+
+php artisan make:model mahasiswa
+php artisan make:migration create_mahasiswas_table
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [DashboardController::class, 'index']);
+
+
+
+
+
+Route::get('/data', function () {
+    return view('data');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
